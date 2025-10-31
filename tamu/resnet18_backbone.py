262b629dacbb57010065b1f096e4f5fb2dd4d0cf
tamuyuri <6323079@ed.tus.ac.jp>
2025-10-31 17:46:42 +0900

    resnet18

--- tamu/resnet18_backbone.py
+++ tamu/resnet18_backbone.py
@@ -223,7 +223,7 @@ class ResNet(nn.Module):
 def _resnet(arch, block, layers, pretrained, progress, **kwargs):
     model = ResNet(block, layers, **kwargs)
 #    if pretrained: 事前学習済み重みをロードする処理などで消去
-#        state_dict = load_state_dict_from_url(model_urls[arch],
+#        state_dict = load_state_dict_from_url(model_urls[arch]
 #                                              progress=progress)
 #        model.load_state_dict(state_dict)
     return model
