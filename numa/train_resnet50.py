ae7cfb0becfaa8cba8eaf41d9f02b9c758064033
sui-2 <ktspw314@gmail.com>
2025-12-03 13:54:38 +0900

    修正

--- numa/train_resnet50.py
+++ numa/train_resnet50.py
@@ -10,6 +10,7 @@ from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt 
 from tqdm import tqdm
 import numpy as np 
+from torch.optim.lr_scheduler import ReduceLROnPlateau
 
 
 
